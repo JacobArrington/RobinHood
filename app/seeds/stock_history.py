9dd3714eeb991f159dd2f410aee94534c6f9a0ad
Jacob Arrington <Jacobsarrington@gmail.com>
2023-05-02 18:21:16 -0400

    Stockhitory seeded

--- app/seeds/stock_history.py
+++ app/seeds/stock_history.py
@@ -1,47 +1,4 @@
-# import random
-# from datetime import datetime, timedelta
-# from app.models import db, Stock, StockHistory, environment, SCHEMA
-# from sqlalchemy.sql import text
 
-# def create_stock_history():
-#     stocks_data = {}
-
-#     # Get all stocks from the database
-#     all_stocks = Stock.query.all()
-
-#     # Generate dates for a calendar year from a year ago
-#     start_date = datetime.now() - timedelta(days=365)
-#     end_date = datetime.now()
-#     dates = [start_date + timedelta(days=i) for i in range((end_date - start_date).days )]
-
-#     # Create a dictionary with stock id as key and list of prices as value
-#     for stock in all_stocks:
-#         stock_prices = []
-#         for date in dates:
-#             price = round(random.uniform(1, 100), 2)
-#             stock_prices.append(price)
-#             key = (stock.id, date.date())
-#             stocks_data[key] = {'stock_id': stock.id, 'date': date, 'price': price}
-            
-#         stocks_data[stock.id] = stock_prices
-        
-
-#     # Insert stock history into the database
-#     for stock_data in stocks_data.values():
-#         stock_history = StockHistory(**stock_data)
-#         db.session.add(stock_history)
-
-#     db.session.commit()
-
-#     #return stocks_data
-
-
-# def undo_stock_history():
-#     if environment == "production":
-#         db.session.execute(f"TRUNCATE table {SCHEMA}.stock_history RESTART IDENTITY CASCADE;")
-#     else:
-#         db.session.execute(text("DELETE FROM stock_history"))
-#     db.session.commit()
 import random
 import random
 from datetime import datetime, timedelta
