34c30ea84b6a507e5603c49d3e2c542bb8008f49
Paul Jha <paul.hutalla@gmail.com>
2023-05-02 16:20:16 -0700

    added stock id route

--- app/api/stocks_routes.py
+++ app/api/stocks_routes.py
@@ -6,4 +6,9 @@ stocks_routes = Blueprint('stocks', __name__)
 @stocks_routes.route('/')
 def get_all_stocks():
     stocks = Stock.query.all()
-    return {'stocks': [stock.to_stock_dict() for stock in stocks]}
+    return [stock.to_stock_dict() for stock in stocks]
+
+@stocks_routes.route('/<int:id>')
+def get_stock_by_id(id):
+    stock = Stock.query.get(id)
+    return stock.to_stock_dict()
