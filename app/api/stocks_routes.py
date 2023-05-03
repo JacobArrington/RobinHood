1554cbf9b917e0ea8af54dd175d5d085dc11a94d
Kelly Kong <kkong1992@gmail.com>
2023-05-02 20:00:44 -0400

    stock_history route

--- app/api/stocks_routes.py
+++ app/api/stocks_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from app.models import Stock
+from app.models import Stock, StockHistory
 
 stocks_routes = Blueprint('stocks', __name__)
 
@@ -12,3 +12,11 @@ def get_all_stocks():
 def get_stock_by_id(id):
     stock = Stock.query.get(id)
     return stock.to_stock_dict()
+
+@stocks_routes.route('/<int:id>/stock_history')
+def get_stock_history(id):
+    stock = Stock.query.get(id)
+    print(stock,'@@@@@@@')
+    stock_history = StockHistory.query.get(stock.id)
+    print(stock_history,'!!!!!!!!!')
+    return stock_history.to_stock_history_dict()
