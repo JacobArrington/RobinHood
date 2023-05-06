aed89d09fe16cface34faba73e6a72f186018823
Jacob Arrington <Jacobsarrington@gmail.com>
2023-05-06 16:33:09 -0400

    added filter to graph

--- react-app/src/components/Graph/chart.js
+++ react-app/src/components/Graph/chart.js
@@ -1,40 +1,89 @@
 import React, { PureComponent } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './chart.css'
+import { useHistory } from 'react-router-dom';
 
-const StockChart = ({stockHistory}) => {
-  if(!stockHistory){
-    return <div>No Stock Avail</div>
+
+const filterDataByTimeframe = (data, timeframe) => {
+  const currentDate = new Date()
+  let filteredData = data
+  switch (timeframe) {
+    case 'daily':
+      filteredData = data.filter((hist) => {
+        const histDate = new Date(hist.date);
+        return currentDate.toLocaleString() === histDate.toDateString()
+      });
+      break;
+    case 'weekly':
+      filteredData = data.filter((hist) => {
+        const histDate = new Date(hist.date);
+        
+        const daysDifference = Math.floor((currentDate - histDate) / (1000 * 60 * 60 * 24));
+        return daysDifference <7
+      })
+      break;
+    case 'monthly':
+      filteredData = data.filter((hist) => {
+        const histDate = new Date(hist.date);
+        return currentDate.getMonth() === histDate.getMonth() && currentDate.getFullYear() === histDate.getFullYear()
+        // checks if the histdate falls in same month and year as the currentDate
+      })
+      break;
+    case 'quarterly':
+      filteredData = data.filter((hist) => {
+        const histDate = new Date(hist.date);
+        const currentQuarter = Math.floor((currentDate.getMonth() + 3) / 3);
+        const histQuarter = Math.floor((histDate.getMonth() + 3) / 3);
+        return currentQuarter === histQuarter && currentDate.getFullYear() === histDate.getFullYear();
+      });
+      break;
+      case 'yearly':
+    filteredData = data.filter((hist) => {
+      const histDate = new Date(hist.date);
+      return currentDate.getFullYear() === histDate.getFullYear();
+    });
+    break;
+  default:
+    break;
+}
+
+return filteredData;
+}
+
+const StockChart = ({ stockHistory, timeframe }) => {
+  if (!stockHistory) {
+    return <div>No Stock Available</div>;
   }
 
-    let data = Object.values(stockHistory).map((history) => ({
-        date: new Date(history.date).toLocaleDateString(),
-        price: history.price
-    }))
-    console.log(data)
+  let data = Object.values(stockHistory).map((history) => ({
+    date: new Date(history.date).toLocaleDateString(),
+    price: history.price,
+  }));
 
-    return (
-      <ResponsiveContainer width={1500} height={750}>
-        <LineChart
-          width={1200}
-          height={600}
-          data={data}
-          margin={{
-            top: 5,
-            right: 40,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 8 }} />
-        </LineChart>
-      </ResponsiveContainer>
-    );
-  }
+  let filteredData = filterDataByTimeframe(data, timeframe);
 
-  export default StockChart
+  return (
+    <ResponsiveContainer width={1500} height={750}>
+      <LineChart
+        width={1200}
+        height={600}
+        data={filteredData}
+        margin={{
+          top: 5,
+          right: 40,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 8 }} />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default StockChart;
--- react-app/src/components/Stocks/index.js
+++ react-app/src/components/Stocks/index.js
@@ -10,7 +10,7 @@ const Stock = () => {
     const allStocks = useSelector((state) => state.stocksReducer);
 
     const [stocks, setStock] = useState(Object.keys(allStocks))
-    
+
 
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -20,6 +20,7 @@ const Stock = () => {
 
     return (
         <div>
+           
             {Object.values(allStocks).map(stock => (
                 <div key={stock.id} onClick={() => setStock(stock)}>
                     {stock.id}
