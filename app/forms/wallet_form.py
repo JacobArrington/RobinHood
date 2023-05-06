ed576745da70c7721a16fa01474796b73a071048
Kelly Kong <kkong1992@gmail.com>
2023-05-06 14:06:58 -0400

    added new routes for wallet

---
+++ app/forms/wallet_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+from app.models import Wallet
+
+class WalletForm(FlaskForm):
+    account_type = StringField('Account Type', validators=[DataRequired()])
+    account_num = IntegerField('Account Number', validators=[DataRequired()])
+    routing_num = IntegerField('Routing Number', validators=[DataRequired()])
+    cash = IntegerField('Cash', validators=[DataRequired()])
