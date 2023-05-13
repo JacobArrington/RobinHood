56df2199f46342adc45b2ad949717c829e0f35eb
Jacob Arrington <Jacobsarrington@gmail.com>
2023-05-13 17:18:29 -0400

    owned_stock

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,6 +15,7 @@ import Transaction from "./components/Transaction";
 import ProtectedDashboard from "./components/Dashboard";
 import Watchlists from "./components/Watchlists";
 import Portfolio from "./components/Portfolio";
+import OwnedStock from "./components/OwnedStock";
 
 function App() {
   const dispatch = useDispatch();
@@ -45,10 +46,12 @@ function App() {
               <Stock />
               {/* <GetStockById /> */}
               <StockHistory />
+              
               <Wallet />
               <Watchlists />
               <Transaction />
               <Portfolio />
+              <OwnedStock />
             </>
           </ProtectedRoute>
         </Switch>
--- react-app/src/components/OwnedStock/index.js
+++ react-app/src/components/OwnedStock/index.js
@@ -1,26 +1,41 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTransaction } from "../../store/transaction";
+import { fetchShares } from "../../store/shares";
+import { fetchStocks } from "../../store/stocks"; 
 
 const OwnedStock = () =>{
-    const shares = useSelector((state) => shares);
+    const dispatch = useDispatch()
+    const userId = useSelector((state) => state.session.user.id)
+    const allTransactions = useSelector((state) => state.transactionReducer)
+    const stocks = useSelector((state) => state.stocksReducer) 
 
-    const sharesList = Object.values(shares)
+    useEffect(()=> {
+        dispatch(fetchTransaction())
+        dispatch(fetchShares())
+        dispatch(fetchStocks())
+    }, [dispatch])
 
+    const userTransactions = Object.values(allTransactions).filter(
+        (transaction) => transaction.user_id === userId
+      );
+      const userOwnedStocks = userTransactions.reduce((acc,transaction) =>{
+        if(!acc[transaction.stock_id]){
+            acc[transaction.stock_id] ={
+                stockName: stocks
+            }
+        }
+      })
+
+  
     return (
         <div>
             <h2>Owned_Stocks</h2>
-            <ul>
-                {sharesList.map((share)=>(
-                    <div key={share.id}>
-                        <h3>{share.stock.name}</h3>
-                        <p>Shares: {share.total.price}</p>
-                        <p>price per: {share.total.price}</p>
-                        <p>Shares: {share.total.price}</p>
-                    </div>
-                ))}
-            </ul>
+           
         </div>
     )
 
     
 }
+
+export default OwnedStock;
