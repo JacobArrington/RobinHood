90e7e816c18366ef8581489d9f42ddcfc9e3de98
Kelly Kong <kkong1992@gmail.com>
2023-05-13 21:12:30 -0400

    edit search res

--- react-app/src/components/SearchResModal/index.js
+++ react-app/src/components/SearchResModal/index.js
@@ -1,20 +1,47 @@
+// import React from 'react';
+// import { useModal } from '../../context/Modal';
+// import "./SearchRes.css"
+
+// export default function SearchResModal({searchResults, onStockSelect}) {
+//     const { closeModal } = useModal();
+
+//     const handleClick = (stockId) => {
+//         onStockSelect(stockId);
+//         closeModal();
+//       };
+//       return(
+//         <div>
+//         {searchResults.map(stock =>(
+//             <div key={stock.id} onClick={() =>handleClick(stock.id)}>
+//                 {stock.name}
+//             </div>
+//         ))}
+//         </div>
+//       )
+// }
+
+
 import React from 'react';
 import { useModal } from '../../context/Modal';
+import "./SearchRes.css"
 
-export default function SearchResModal({searchResults, onStockSelect}) {
-    const { closeModal } = useModal();
+export default function SearchResModal({ searchResults, onStockSelect }) {
+  const { closeModal } = useModal();
 
-    const handleClick = (stockId) => {
-        onStockSelect(stockId);
-        closeModal();
-      };
-      return(
-        <div>
-        {searchResults.map(stock =>(
-            <div key={stock.id} onClick={() =>handleClick(stock.id)}>
-                {stock.name}
-            </div>
+  const handleClick = (stockId) => {
+    onStockSelect(stockId);
+    closeModal();
+  };
+
+  return (
+    <div className="search-res-modal">
+      <div className="search-res-list">
+        {searchResults.slice(0, 10).map((stock) => (
+          <div key={stock.id} onClick={() => handleClick(stock.id)}>
+            {stock.name}
+          </div>
         ))}
-        </div>
-      )
+      </div>
+    </div>
+  );
 }
