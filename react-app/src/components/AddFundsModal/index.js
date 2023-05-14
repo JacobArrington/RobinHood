ec4b778e7dd8823b83fceb4f8358081045a9b9ce
Marlon Santos <santosfmarlon@gmail.com>
2023-05-13 20:49:20 -0400

    added adding_funds function

---
+++ react-app/src/components/AddFundsModal/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useModal } from "../../context/Modal";
+import { changeBuyingPower } from "../../store/portfolio";
+
+function AddFundsModal() {
+    const dispatch = useDispatch()
+    let buyingPower = useSelector((state) => (state.portfolioReducer.portfolio.buyingPower))
+    const portfolio = useSelector((state)=> (state.portfolioReducer.portfolio))
+    const [totalFunds, setTotalFunds] = useState(0);
+    const [errors, setErrors] = useState([]);
+    const { closeModal } = useModal();
+
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+          const buyingpowerData = {
+            ...portfolio,
+            buyingPower: Number(buyingPower) + Number(totalFunds)
+        }
+
+        const transfer = await dispatch(changeBuyingPower(portfolio.id, buyingpowerData))
+        if (transfer) {
+            setErrors(transfer)
+        } else {
+            closeModal();
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                How much would you like to add to your account?
+                <input
+                    type="number"
+                    value={totalFunds}
+                    onChange={(e) => setTotalFunds(e.target.value)}
+                />
+            </label>
+
+            <label>
+                New total buying power:{Number(buyingPower) + Number(totalFunds)}
+            </label>
+            <button type="submit" disabled={totalFunds < 0}>Add funds</button>
+        </form>
+    );
+}
+
+export default AddFundsModal;
--- react-app/src/components/OwnedStock/index.js
+++ react-app/src/components/OwnedStock/index.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTransaction } from "../../store/transaction";
-import { fetchShares } from "../../store/shares";
-import { fetchStocks } from "../../store/stocks"; 
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchTransaction } from "../../store/transaction";
+// import { fetchShares } from "../../store/shares";
+// import { fetchStocks } from "../../store/stocks"; 
 
-const OwnedStock = () =>{
-    const dispatch = useDispatch()
-    const userId = useSelector((state) => state.session.user.id)
-    const allTransactions = useSelector((state) => state.transactionReducer)
-    const stocks = useSelector((state) => state.stocksReducer) 
+// const OwnedStock = () =>{
+//     const dispatch = useDispatch()
+//     const userId = useSelector((state) => state.session.user.id)
+//     const allTransactions = useSelector((state) => state.transactionReducer)
+//     const stocks = useSelector((state) => state.stocksReducer) 
 
-    useEffect(()=> {
-        dispatch(fetchTransaction())
-        dispatch(fetchShares())
-        dispatch(fetchStocks())
-    }, [dispatch])
+//     useEffect(()=> {
+//         dispatch(fetchTransaction())
+//         dispatch(fetchShares())
+//         dispatch(fetchStocks())
+//     }, [dispatch])
 
-    const userTransactions = Object.values(allTransactions).filter(
-        (transaction) => transaction.user_id === userId
-      );
-      const userOwnedStocks = userTransactions.reduce((acc,transaction) =>{
-        if(!acc[transaction.stock_id]){
-            acc[transaction.stock_id] ={
-                stockName: stocks
-            }
-        }
-      })
+//     const userTransactions = Object.values(allTransactions).filter(
+//         (transaction) => transaction.user_id === userId
+//       );
+//       const userOwnedStocks = userTransactions.reduce((acc,transaction) =>{
+//         if(!acc[transaction.stock_id]){
+//             acc[transaction.stock_id] ={
+//                 stockName: stocks
+//             }
+//         }
+//       })
 
   
-    return (
-        <div>
-            <h2>Owned_Stocks</h2>
+//     return (
+//         <div>
+//             <h2>Owned_Stocks</h2>
            
-        </div>
-    )
+//         </div>
+//     )
 
     
-}
+// }
 
-export default OwnedStock;
+// export default OwnedStock;
