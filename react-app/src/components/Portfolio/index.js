94cfad488b47c0f8bb744b8066adfa50d5bc14b9
Kelly Kong <kkong1992@gmail.com>
2023-05-14 20:08:15 -0400

    updated some styling for wallet

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -1,26 +1,59 @@
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Redirect } from "react-router-dom";
+// // import UpdateWalletModal from "../UpdateWalletModal"
+// // import PostWalletModal from "../PostWalletModal";
+// // import OpenModalButton from "../OpenModalButton";
+// import { fetchPortfolio } from "../../store/portfolio";
+// import './Portfolio.css'
+
+// const Portfolio = () => {
+//     const dispatch = useDispatch()
+//     const portfolioState = useSelector((state) => state.portfolioReducer)
+//     useEffect(() => {
+//         dispatch(fetchPortfolio())
+//     }, [dispatch])
+
+//     return (
+//         <>
+//         <p>{portfolioState?.portfolio?.user}</p>
+//         <p>{portfolioState?.portfolio?.wallet}</p>
+//         <p>${portfolioState?.portfolio?.buyingPower}: Buying Power</p>
+//        </>
+//     )
+// }
+
+// export default Portfolio;
+
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-// import UpdateWalletModal from "../UpdateWalletModal"
-// import PostWalletModal from "../PostWalletModal";
-// import OpenModalButton from "../OpenModalButton";
 import { fetchPortfolio } from "../../store/portfolio";
-import './Portfolio.css'
+import "./Portfolio.css";
 
 const Portfolio = () => {
-    const dispatch = useDispatch()
-    const portfolioState = useSelector((state) => state.portfolioReducer)
-    useEffect(() => {
-        dispatch(fetchPortfolio())
-    }, [dispatch])
+  const dispatch = useDispatch();
+  const portfolioState = useSelector((state) => state.portfolioReducer);
 
-    return (
-        <>
-        <p>{portfolioState?.portfolio?.user_id}</p>
-        <p>{portfolioState?.portfolio?.wallet}</p>
-        <p>{portfolioState?.portfolio?.buyingPower}</p>
-       </>
-    )
-}
+  useEffect(() => {
+    dispatch(fetchPortfolio());
+  }, [dispatch]);
+
+  return (
+    <div className="portfolio-container">
+      <h2 className="portfolio-title">{portfolioState?.portfolio?.user}</h2>
+      <div className="grid-container">
+        {/* <div className="grid-item wallet"> */}
+          {/* <h3 className="grid-item-title">Wallet</h3> */}
+          {/* <p className="grid-item-value">${portfolioState?.portfolio?.wallet}</p> */}
+        {/* </div>
+        <div className="grid-item buying-power">
+          <h3 className="grid-item-title">Buying Power</h3>
+          <p className="grid-item-value">${portfolioState?.portfolio?.buyingPower}</p>
+        </div> */}
+      </div>
+    // </div>
+  );
+};
 
 export default Portfolio;
--- react-app/src/components/Wallet/index.js
+++ react-app/src/components/Wallet/index.js
@@ -1,6 +1,100 @@
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Redirect } from "react-router-dom";
+// import UpdateWalletModal from "../UpdateWalletModal"
+// import AddFundsModal from "../AddFundsModal"
+// import PostWalletModal from "../PostWalletModal";
+// import OpenModalButton from "../OpenModalButton";
+// import { fetchWallet, destroyWallet, updateUserWallet } from "../../store/wallet";
+// import './wallet.css'
+
+// const Wallet = () => {
+//     const dispatch = useDispatch()
+//     const allWallets = useSelector((state) => state.walletReducer);
+//     console.log(allWallets)
+
+//     const [isLoaded, setIsLoaded] = useState(false);
+
+//     const deleteWallet = (walletId) => {
+//         const confirm = window.confirm(
+//             'Are you sure? This action cannot be undone'
+//         )
+//         if(confirm){
+//             dispatch(destroyWallet(walletId))
+//         }
+
+//     }
+
+//     const fetchUpdateWallet = async () => {
+//         await dispatch(fetchWallet())
+//     }
+
+//     const updateUserWallet = (id, updateWalletData) => {
+//         dispatch(updateUserWallet(id, updateWalletData))
+//     }
+
+//     useEffect(() => {
+//         dispatch(fetchWallet()).then(() => setIsLoaded(true));
+//     }, [dispatch]);
+
+
+
+//     const isWalletsEmpty = Object.keys(allWallets).length === 0;
+
+//     return (
+//         <div>
+//           {!isWalletsEmpty ? (
+
+//             <>
+//               {Object.values(allWallets).map((wallet) => (
+//                 <div key={wallet.id}>
+//                   <p>{wallet.account_type}</p>
+//                   <p>{wallet.routing_num}</p>
+//                   <button onClick={() => deleteWallet(wallet.id)}>
+//                     Delete Wallet
+//                   </button>
+//                   <OpenModalButton
+//                     buttonText="Update Wallet"
+//                     modalComponent={
+//                       <UpdateWalletModal
+//                         wallet={wallet}
+//                         fetchUpdateWallet={fetchUpdateWallet}
+//                       />
+//                     }
+//                   />
+//                   <OpenModalButton
+//                     buttonText="Add funds"
+//                     modalComponent={
+//                       <AddFundsModal />
+//                     }
+//                   />
+//                 </div>
+//               ))}
+//             </>
+//           ) : (
+//             <div>
+//             <p>no wallet found please add one</p>
+//             <OpenModalButton
+//               buttonText="Create Wallet"
+//               modalComponent={
+//                 <PostWalletModal fetchUpdateWallet={fetchUpdateWallet} />
+
+//               }
+//             />
+//             </div>
+//           )}
+//         </div>
+//       );
+//             }
+
+// export default Wallet;
+
+
+
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import Portfolio from "../Portfolio";
 import UpdateWalletModal from "../UpdateWalletModal"
 import AddFundsModal from "../AddFundsModal"
 import PostWalletModal from "../PostWalletModal";
@@ -9,83 +103,84 @@ import { fetchWallet, destroyWallet, updateUserWallet } from "../../store/wallet
 import './wallet.css'
 
 const Wallet = () => {
-    const dispatch = useDispatch()
-    const allWallets = useSelector((state) => state.walletReducer);
-    console.log(allWallets)
+  const dispatch = useDispatch()
+  const allWallets = useSelector((state) => state.walletReducer);
+  const portfolioState = useSelector((state) => state.portfolioReducer);
+  console.log(allWallets)
 
-    const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    const deleteWallet = (walletId) => {
-        const confirm = window.confirm(
-            'Are you sure? This action cannot be undone'
-        )
-        if(confirm){
-            dispatch(destroyWallet(walletId))
-        }
-        
+  const deleteWallet = (walletId) => {
+    const confirm = window.confirm(
+        'Are you sure? This action cannot be undone'
+    )
+    if(confirm){
+        dispatch(destroyWallet(walletId))
     }
 
-    const fetchUpdateWallet = async () => {
-        await dispatch(fetchWallet())
-    }
+  }
 
-    const updateUserWallet = (id, updateWalletData) => {
-        dispatch(updateUserWallet(id, updateWalletData))
-    }
+  const fetchUpdateWallet = async () => {
+    await dispatch(fetchWallet())
+  }
 
-    useEffect(() => {
-        dispatch(fetchWallet()).then(() => setIsLoaded(true));
-    }, [dispatch]);
+  const updateUserWallet = (id, updateWalletData) => {
+    dispatch(updateUserWallet(id, updateWalletData))
+  }
 
-  
+  useEffect(() => {
+    dispatch(fetchWallet()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
-    const isWalletsEmpty = Object.keys(allWallets).length === 0;
+  const isWalletsEmpty = Object.keys(allWallets).length === 0;
 
-    return (
-        <div>
-          {!isWalletsEmpty ? (
-           
-            <>
-             {console.log(allWallets, '!!!!!!!!!!!!!!!!')}
-              {Object.values(allWallets).map((wallet) => (
-                <div key={wallet.id}>
-                  <p>{wallet.account_type}</p>
-                  <p>{wallet.routing_num}</p>
-                  <button onClick={() => deleteWallet(wallet.id)}>
-                    Delete Wallet
-                  </button>
-                  <OpenModalButton
-                    buttonText="Update Wallet"
-                    modalComponent={
-                      <UpdateWalletModal
-                        wallet={wallet}
-                        fetchUpdateWallet={fetchUpdateWallet}
-                      />
-                    }
-                  />
-                  <OpenModalButton
-                    buttonText="Add funds"
-                    modalComponent={
-                      <AddFundsModal />
-                    }
-                  />
-                </div>
-              ))}
-            </>
-          ) : (
-            <div>
-            <p>no wallet found please add one</p>
+  return (
+    <div className="wallet-container">
+      <div className="wallet-list">
+        {!isWalletsEmpty ? (
+          <>
+            {Object.values(allWallets).map((wallet) => (
+              <div key={wallet.id} className="wallet-card">
+                <p>Account Type: {wallet.account_type}</p>
+                <p>Routing # {wallet.routing_num}</p>
+                <button onClick={() => deleteWallet(wallet.id)}>
+                  Delete Wallet
+                </button>
+                <OpenModalButton
+                  buttonText="Update Wallet"
+                  modalComponent={
+                    <UpdateWalletModal
+                      wallet={wallet}
+                      fetchUpdateWallet={fetchUpdateWallet}
+                    />
+                  }
+                />
+                <OpenModalButton
+                  buttonText="Add funds"
+                  modalComponent={
+                    <AddFundsModal />
+                  }
+                />
+              </div>
+            ))}
+          </>
+        ) : (
+          <div>
+            <p>No wallet found. Please add one.</p>
             <OpenModalButton
               buttonText="Create Wallet"
               modalComponent={
                 <PostWalletModal fetchUpdateWallet={fetchUpdateWallet} />
-                
               }
             />
-            </div>
-          )}
-        </div>
-      );
-            }
+          </div>
+        )}
+      </div>
+      <div className="buying-power">
+        <p>${portfolioState?.portfolio?.buyingPower}: Buying Power</p>
+      </div>
+    </div>
+  );
+}
 
 export default Wallet;
